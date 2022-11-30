04f99ff0e9cbb5024bbeb05ea45269f7acd08461
Bilal <blarose@dubai-digitalfactory.com>
2022-11-30 23:27:46 +0100

    version final

--- Tech Challenger S1E3 (qualif).playground/Contents.swift
+++ Tech Challenger S1E3 (qualif).playground/Contents.swift
@@ -10,69 +10,69 @@ func load(file named: String) -> String? {
     return content
 }
 
-
-let nbrExo = 2
+let nbrExo = 3
 
 let line = load(file: "input\(nbrExo)")!
 
-struct Voyage {
-    let hoursDepart: String
-    let empreinte: Int
-}
-
 let dateFormater = DateFormatter()
 dateFormater.dateFormat = "HH:mm:ss"
-
 var sLine = line
 let nbrTotalVoyage = sLine.split(separator: "\n").first
 sLine.removeFirst(nbrTotalVoyage!.count + 2)
 
+let allerStr = "Paris-Lyon"
+let retourStr = "Lyon-Paris"
+
 let horaireDuJour = sLine.split(separator: "\n")
 
+let allerBrut = horaireDuJour.filter{ $0.contains(allerStr)}
+let retourBrut = horaireDuJour.filter{ $0.contains(retourStr)}
 
-var voyagesAller = [Voyage]()
-var voyagesRetour = [Voyage]()
-
-horaireDuJour.forEach { item in
-    let voyageArray = item.split(separator: " ")
+let retourSorted = retourBrut.sorted(by: {
+    let item0 = $0.split(separator: " ")
+    let item1 = $1.split(separator: " ")
     
-    let hoursDepart = String(voyageArray.first!)
-    let cityDepart = String(voyageArray[1])
-    let empreinte = String(voyageArray.last!)
+    let firstempreinte = Int(item0.last!)!
+    let secondempreinte = Int(item1.last!)!
+    
+    return firstempreinte < secondempreinte })
 
-    if cityDepart == "Paris-Lyon" { // ALLER
-        voyagesAller.append(Voyage(hoursDepart: hoursDepart,
-                                   empreinte: Int(empreinte)!))
-    } else { // Retour
-        voyagesRetour.append(Voyage(hoursDepart: hoursDepart,
-                                    empreinte: Int(empreinte)!))
-    }
-}
-
-let firstDepart = voyagesAller.sorted(by: {dateFormater.date(from: $0.hoursDepart)! < dateFormater.date(from: $1.hoursDepart)!}).first!
-let lastRetour = voyagesRetour.sorted(by: { dateFormater.date(from: $0.hoursDepart)! < dateFormater.date(from: $1.hoursDepart)!}).last!
-
-let allerClean = voyagesAller.filter { dateFormater.date(from: $0.hoursDepart)! < dateFormater.date(from: lastRetour.hoursDepart)! }
-
-let retourClean = voyagesRetour.filter { dateFormater.date(from: $0.hoursDepart)! > dateFormater.date(from: firstDepart.hoursDepart)! }
-
-let allerSorted = allerClean.sorted(by: { $0.empreinte < $1.empreinte })
-
-let retourSorted = retourClean.sorted(by: { $0.empreinte < $1.empreinte })
-
+let allerSorted = allerBrut.sorted(by: {
+    let item0 = $0.split(separator: " ")
+    let item1 = $1.split(separator: " ")
+    
+    let firstTimeStr = item0.last!
+    let secondTimeStr = item1.last!
+    
+    let firstEmpreinte = Int(firstTimeStr)!
+    let secondEmpreinte = Int(secondTimeStr)!
+    return firstEmpreinte < firstEmpreinte })
 
 var probable = Int.max
 allerSorted.forEach { aller in
-    guard aller.empreinte < probable else { return }
+    print("test:----\(aller)")
+    let aItem = aller.split(separator: " ")
+    let aEmpreinte = Int(aItem.last!)!
+    guard aEmpreinte < probable else { return }
     for retour in retourSorted {
-        let total = retour.empreinte + aller.empreinte
+
+        let rItem = retour.split(separator: " ")
+        let aDateStr = aItem.first!
+        let rDateStr = rItem.first!
+
+        guard dateFormater.date(from: String(aDateStr))! < dateFormater.date(from: String(rDateStr))! else { continue }
+        
+        let rEmpreinte = Int(rItem.last!)!
+        let total = rEmpreinte + aEmpreinte
         guard total < probable else { return }
-        let hourAller = dateFormater.date(from: aller.hoursDepart)!
-        let hourRetour = dateFormater.date(from: retour.hoursDepart)!
-        guard hourAller.compare(hourRetour) == .orderedAscending else { return }
+        
         probable = total
     }
 }
 
-print(probable)
-print("corigé: \(load(file: "output\(nbrExo)")!)")
+let correction = Int(load(file: "output\(nbrExo)")!)
+if correction == probable {
+    print("Bravo tu a réussit le test")
+} else {
+    print("Helas, tu devait trouver: \(correction!); tu a trouvé: \(probable)")
+}
